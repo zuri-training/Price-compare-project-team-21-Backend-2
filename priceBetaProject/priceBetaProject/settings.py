c69279bc64fb1c55222fd82fe8c3fb67f6ba6b57
Afeh <balogunafebu22@gmail.com>
2022-07-30 14:17:56 +0100

    Made corrections, except index.html

--- priceBetaProject/priceBetaProject/settings.py
+++ priceBetaProject/priceBetaProject/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'priceBetaProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "index"
