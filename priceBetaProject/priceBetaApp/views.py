b15a7506415831deaa3cd16af6fc2c95e48c09be
Ayodele Ebudola <85792183+DolaseunAyo@users.noreply.github.com>
2022-07-30 09:59:42 +0100

    Update views.py

--- priceBetaProject/priceBetaApp/views.py
+++ priceBetaProject/priceBetaApp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def index(request):
+     user = request.user
+     return render(request,'index.html')
