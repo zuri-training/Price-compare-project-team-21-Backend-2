0d6cc5b722857b59ca7de5c6297bad94eecdf671
unknown <paul.adeboye@yahoo.com>
2022-08-02 08:21:59 +0100

    UPDATED MODELS, ADDED A MEDIA FOLDER, REGISTERED THE NEW MODELS ON ADMIN.
    -I updated the models.py to include models for Product, Categories, Store and Wishlist.
    -Added a media folder and configured the media folder on settings.py and urls.py(PriceBetaProject) to inform Django about the media folder that was created.
    -Registered the new models on admin.py
    -Updated the views.py

--- priceBetaProject/priceBetaProject/settings.py
+++ priceBetaProject/priceBetaProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
 
     #Created apps
     'Accounts',
+    'priceBetaApp',
     
 ]
 
@@ -129,3 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "index"
 LOGOUT_REDIRECT_URL = "index"
+
+# Informing Django about the media folder that was created
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- priceBetaProject/priceBetaProject/urls.py
+++ priceBetaProject/priceBetaProject/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,9 @@ urlpatterns = [
     path("accounts/", include("Accounts.urls")),
     path("", include("priceBetaApp.urls")),
 ]
+
+
+
+# Configuring the media folder 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings    )
